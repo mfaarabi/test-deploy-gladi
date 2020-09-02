c7362c5c8b3d9fe3f3f471ce9c9b9d8a088241fa
Muhammad Farabi <muhammad.farabi@go-jek.com>
2020-09-02 23:30:12 +0700

    Implement form with state, handleSubmit, and edit profile

--- src/components/profile/edit/index.js
+++ src/components/profile/edit/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProfileEdit = () => (
-  <>
-    <label htmlFor="name">Name</label>
-    <input id="name" type="text" />
-    <label htmlFor="username">Username</label>
-    <input id="username" type="text" />
-    <label htmlFor="bio">Bio</label>
-    <input id="bio" type="text" />
-    <button>Save</button>
-  </>
-);
+const ProfileEdit = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [bio, setBio] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert(`name: ${name}, username: ${username}, bio: ${bio}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        value={name}
+        onChange={({ target: { value } }) => setName(value)}
+      />
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        type="text"
+        value={username}
+        onChange={({ target: { value } }) => setUsername(value)}
+      />
+      <label htmlFor="bio">Bio</label>
+      <input
+        id="bio"
+        type="text"
+        value={bio}
+        onChange={({ target: { value } }) => setBio(value)}
+      />
+      <button>Save</button>
+    </form>
+  );
+};
 
 export default ProfileEdit;
