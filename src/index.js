f41aa1d1e7f33898818aea19d581942d47070bf1
Muhammad Farabi <muhammad.farabi@go-jek.com>
2020-09-02 11:06:03 +0700

    Refactor to use App component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const name = 'Dave';
-const heading = 'h2';
-const div = <div>Hello {name}!</div>;
-const HelloWorld = ({ heading }) =>
-  heading === 'h1' ? <h1>{div}</h1> : <h2>{div}</h2>;
 ReactDOM.render(
-  <HelloWorld heading={heading} />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
