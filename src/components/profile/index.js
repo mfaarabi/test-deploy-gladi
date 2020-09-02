4f7480a88a5fdc310944f042be055109c44b4ccb
Muhammad Farabi <muhammad.farabi@go-jek.com>
2020-09-02 16:52:11 +0700

    Introduce state, event handler, and basic conditional rendering

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Profile = () => {
   const username = 'Dave';
   const postCount = 0;
+  const [isFollowed, setIsFollowed] = useState(false);
 
   return (
     <>
       <h1>Profile of {username}</h1>
       <h2>{postCount} Posts</h2>
+      <button
+        onClick={() => {
+          setIsFollowed(!isFollowed);
+        }}
+      >
+        {isFollowed ? 'Unfollow' : 'Follow'}
+      </button>
     </>
   );
 };
