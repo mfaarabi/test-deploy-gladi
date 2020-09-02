a280d42de5f296267c36c35b25288c6b9dad60d8
Muhammad Farabi <muhammad.farabi@go-jek.com>
2020-09-02 10:48:29 +0700

    Introducing component and props

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import * as serviceWorker from './serviceWorker';
 const name = 'Dave';
 const heading = 'h2';
 const div = <div>Hello {name}!</div>;
-const helloWorld = heading === 'h1' ? <h1>{div}</h1> : <h2>{div}</h2>;
-ReactDOM.render(helloWorld, document.getElementById('root'));
+const HelloWorld = ({ heading }) =>
+  heading === 'h1' ? <h1>{div}</h1> : <h2>{div}</h2>;
+ReactDOM.render(
+  <HelloWorld heading={heading} />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
