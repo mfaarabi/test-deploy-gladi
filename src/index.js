3cc69d0c168aa0949538b7035e71cfe64bf7b219
Muhammad Farabi <muhammad.farabi@go-jek.com>
2020-09-02 10:06:35 +0700

    Introducing JSX and its capabilities

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const helloWorld = 'Hello World!';
+const name = 'Dave';
+const heading = 'h2';
+const div = <div>Hello {name}!</div>;
+const helloWorld = heading === 'h1' ? <h1>{div}</h1> : <h2>{div}</h2>;
 ReactDOM.render(helloWorld, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
