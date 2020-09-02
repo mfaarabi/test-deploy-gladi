23dc51b30eee5a31ac5cd5c306096fbcdce3f95f
Muhammad Farabi <muhammad.farabi@go-jek.com>
2020-09-02 16:06:23 +0700

    Destructure props inside function argument

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-const Profile = props => {
-  const { username } = props;
-  return <h1>Profile of {username}</h1>;
-};
+const Profile = ({ username }) => <h1>Profile of {username}</h1>;
 
 Profile.propTypes = {
   username: String
