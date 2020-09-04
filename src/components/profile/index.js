3a0c1ed14722c7c2f145b2f87c622dffcbf0dd59
Muhammad Farabi <muhammad.farabi@go-jek.com>
2020-09-04 13:17:47 +0700

    Center elements

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -11,40 +11,42 @@ const Profile = () => {
   const auth = true;
 
   return (
-    <div className={styles.main}>
-      <div className={styles.profile}>
-        <div>
-          <img
-            className={styles.profilePictureImg}
-            src="https://bit.ly/dan-abramov"
-            alt="Dan Abramov"
-          />
+    <div className={styles.center}>
+      <div className={styles.main}>
+        <div className={styles.profile}>
+          <div className={styles.center}>
+            <img
+              className={styles.profilePictureImg}
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+          </div>
+          <div>
+            <h1>@{username}</h1>
+            <h2>{postCount} Posts</h2>
+            {auth ? (
+              <Link to="/profile/edit">Edit Profile</Link>
+            ) : (
+              <button
+                onClick={() => {
+                  setIsFollowed(!isFollowed);
+                }}
+              >
+                {isFollowed ? 'Unfollow' : 'Follow'}
+              </button>
+            )}
+          </div>
         </div>
-        <div>
-          <h1>@{username}</h1>
-          <h2>{postCount} Posts</h2>
-          {auth ? (
-            <Link to="/profile/edit">Edit Profile</Link>
-          ) : (
-            <button
-              onClick={() => {
-                setIsFollowed(!isFollowed);
-              }}
-            >
-              {isFollowed ? 'Unfollow' : 'Follow'}
-            </button>
-          )}
-        </div>
+        {postCount === 0 ? (
+          <div>no posts</div>
+        ) : (
+          <div className={styles.posts}>
+            {[...Array(postCount)].map((_, idx) => (
+              <Post src="https://via.placeholder.com/300" key={idx} />
+            ))}
+          </div>
+        )}
       </div>
-      {postCount === 0 ? (
-        <div>no posts</div>
-      ) : (
-        <div className={styles.posts}>
-          {[...Array(postCount)].map((_, idx) => (
-            <Post src="https://via.placeholder.com/300" key={idx} />
-          ))}
-        </div>
-      )}
     </div>
   );
 };
